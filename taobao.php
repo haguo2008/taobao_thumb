4575066812c71f9abe7fcf3bea157347a657e477
taoliujun <290762903@qq.com>
2015-06-17 23:04:50 +0800

    新增对淘宝详情部分的图片抓取

--- taobao.php
+++ taobao.php
@@ -1,17 +1,54 @@
 <?php
 $url = $_GET['url'];
+$type = $_GET['type'];
 
-$url = parse_url($url);
-parse_str($url['query'] , $url);
 
-$url = 'http://tbskip.taobao.com/auction/storegallery/item_xml.htm?item_id=' . $url['id'];
-
-$xmlstr = file_get_contents($url);
-$xmlstr = trim($xmlstr);
-$xmlstr = simplexml_load_string($xmlstr);
-$ret = array();
-foreach($xmlstr->item->images->image as $row)
+if( $type === 'thumb' )
 {
-	$ret[] = ((string)$row->url);
+	$url = parse_url($url);
+	parse_str($url['query'] , $url);
+	$url = 'http://tbskip.taobao.com/auction/storegallery/item_xml.htm?item_id=' . $url['id'];
+	$xmlstr = file_get_contents($url);
+	$xmlstr = trim($xmlstr);
+	$xmlstr = simplexml_load_string($xmlstr);
+	$ret = array();
+	foreach($xmlstr->item->images->image as $row)
+	{
+		$ret[] = ((string)$row->url);
+	}
+	exit(json_encode($ret));
 }
-exit(json_encode($ret));
+elseif( $type === 'detail' )
+{
+	$html = file_get_contents($url);
+	$url = parse_url($url);
+	$url2 = null;
+	if( strpos($url['host'],'taobao.com') !== false )
+	{
+		//taobao
+		preg_match('/\"https:\"\s===\slocation\.protocol\s\?\s\"([^\"]+)\"/is' , $html, $preg);
+		if($preg)
+		{
+			$url2 = $preg[1];
+		}
+	}
+	elseif( strpos($url['host'],'tmall.com') !== false )
+	{
+		//taobao
+		preg_match('/\"descUrl\"\:\"([^\"]+)\"/is' , $html, $preg);
+		if($preg)
+		{
+			$url2 = $preg[1];
+		}
+	}
+	if($url2)
+	{
+		$html = file_get_contents('http:'.$url2);
+		preg_match_all('/<img[\s\S]+?src=\"([^\"]+)\"?/is',$html,$preg);
+		if( $preg )
+		{
+			$ret = $preg[1];
+			exit(json_encode($ret));
+		}
+	}
+}
